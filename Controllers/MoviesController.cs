c4a7e327d42c5d68cc73732d2105ca0e343873f4
Emad khan (Home PC) <Spidergrey.emad@gmail.com>
2020-07-15 13:16:28 +0500

    Validate Anti-Forgery Token Added

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -101,6 +101,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (!ModelState.IsValid)
